410203a28f73c8deb188cf0c1a61e1a1337edd2d
mavxyz <82008811+mavxyz@users.noreply.github.com>
2023-08-18 11:55:07 -0500

    feat: initial commit

---
+++ src/main/java/com/svantle/score/SCore.java
@@ -0,0 +1,17 @@
+package com.svantle.score;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
